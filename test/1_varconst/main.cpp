8c5c6f45d6437c548e00e9c69e7e0366d2652036
ramakant921 <ramakantm921@gmail.com>
2025-08-08 15:47:18 +0000

    main

---
+++ test/1_varconst/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int score;
+    score = 110;
+
+    const int uid =569380;
+
+    int spodormonbalance = 50000;
+    spodormonbalance = 8980423;
+
+
+    // uid =83457;
+    cout <<"spodormon"<<endl;
+
+    
+    cout <<"spodormon 2"<<endl;
+    cout <<"spodormon 3"<<endl;
+
+    return 0;
+}
