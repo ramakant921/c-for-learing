eb5deb626d673d2068b2fe34ab5fc81f029b53bd
Ramakant Mishra <ramakantm921@gmail.com>
2025-08-13 23:46:35 +0530

    task

---
+++ test/5_loops/taskone.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int teaCups;
+
+    cout << "Enter the number of tea cups to serve: ";
+    cin >> teaCups;
+
+    return 0;
+}
