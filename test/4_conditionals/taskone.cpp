5121f5a0f4e15bae78c687f9a0387b2196316ee0
Ramakant Mishra <ramakantm921@gmail.com>
2025-08-13 11:55:02 +0530

    task

--- test/4_conditionals/taskone.cpp
+++ test/4_conditionals/taskone.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 
 int main(){
-    
+    string teaOrder;
+
+    cout << "Enter your tea order :";
+    getline(cin, teaOrder);
+
+    if(teaOrder == "Green Tea"){
+    cout << "You have ordered Green Tea" << endl;
+
+    } else{
+        cout << "You have not ordered Green Tea" << endl;
+    }
 
     return 0;
 }
